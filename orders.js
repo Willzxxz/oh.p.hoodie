699cb343acb37e8ba4b428f657fc9b6d802edc8b
Willzxxz <willzxxz@gmail.com>
2022-04-16 20:54:43 -0300

    Dashboard
    finished dashboard with JS scripts

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'iphone x',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Super Giant Drone',
+        productNumber: '32158',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Blue astro Drone',
+        productNumber: '71121',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone with laser',
+        productNumber: '45619',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: '7k max Drone',
+        productNumber: '22325',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
